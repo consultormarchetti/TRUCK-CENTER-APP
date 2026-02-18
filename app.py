fb88d70f052c8b05a9094983a76e5e5f3deac496
consultormarchetti <consultormarchetti@gmail.com>
2026-02-18 10:26:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ URL_PLANILHA = "https://docs.google.com/spreadsheets/d/1o-t_0CWSwMQvVblb-G-9-LBb
 # --- CONFIGURAÇÃO DA IA ---
 try:
     genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
-    model = genai.GenerativeModel('models/gemini-2.0-flash')
+    model = genai.GenerativeModel('models/gemini-2.0-flash-lite')
 except:
     st.error("Erro na chave da IA nos Secrets.")
 
